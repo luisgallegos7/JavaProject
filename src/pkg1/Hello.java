5560af76530d0ae6c8b6f0d943442326ce5a337d
luis2 <luis2@172.18.230.232>
2023-08-25 10:16:22 -0500

    test

---
+++ src/pkg1/Hello.java
@@ -0,0 +1,18 @@
+package pkg1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a;
+		String b;
+		
+		
+		a = 20;
+		b = "Text";
+		
+		System.out.println(a);
+		
+
+	}
+
+}
